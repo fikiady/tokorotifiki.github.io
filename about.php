f04c3f8155c5bbad2390d70280f61f37a58441ab
fikiady <fikiadyansyah@gmail.com>
2024-10-31 20:53:13 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #852d05"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Fiki Cake & Bakery adalah rantai toko roti Indonesia terkemuka dengan 22 cabang (dapur pusat) yang mengelola lebih dari 400 outlet: di Indonesia (Gajah Mada, Pondok Pinang, Jatinegara, Cikini, Sunter, Serpong, Ciputat, Pekayon, Bogor, Karawang, dan Kudus), Bandung, Surabaya, Lampung, Batam, Pekanbaru, Makassar, Manado, Bali, Solo, Semarang, Balikpapan, dan Samarinda. Kami masih terus memperluas secara nasional ke kota-kota lain.</p>
+<p>
+<fieldset></fieldset> Fiki Cake & Bakery adalah salah satu pelopor pertama dalam bisnis roti modern di Indonesia. Didirikan pada tahun 1978, dan hingga tahun 2024, perusahaan ini terus berkembang pesat. Produk kami sehat, bergizi, dan terjangkau oleh semua orang.</p>
+</div>
+
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
---
+++ footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid #852d05;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #852d05"><b>FIKI-CAKE BAKERY</b></h3>
+					<p>Jl. Muria Indah No. F 64 Kec. Bae, Kab. Kudus</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6285886031844</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> fiki-cake@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #852d05; padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; Fiki Ady Ansyah_202151176</span>
+		</div>
+	</footer>
+
+</body>
+</html>
